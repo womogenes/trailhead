481f63ef4cdc44a34c804c61e212b6064a650908
Kshitij Sodani <kshitij@KW09YQYHY7.tld>
2025-02-15 17:45:19 -0800

    added titles and descriptions to resources

--- backend/search_resources.py
+++ backend/search_resources.py
@@ -4,25 +4,19 @@ import json
 class AnswerFormat(BaseModel):
     content_links: str
 
-""""messages": [
-            {"role": "system", "content": "Be precise and concise and don't repeat."},
-            {"role": "user", "content": (
-                "Interesting resources to learn more about "+topic,
-                "Please output a JSON object containing the following fields: "+media_prompt,
-            )},
-        ],"""
+
 def tutorials(topic,media):
     #topic should start with topic. and then later have info. Need to learn more about x,y,z. Already know about a,b,c
     #media is list of kinds of media. example  ["books","podcasts","videos"]
     
-    media_prompt=','.join([i+" links" for i in media])
+    media_prompt=','.join([i+" links,title and brief description" for i in media])
 
     url = "https://api.perplexity.ai/chat/completions"
     headers = {"Authorization": "Bearer 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"}
     payload = {
         "model": "sonar",
         "messages": [
-            {"role": "system", "content": "Be precise and concise and don't repeat. Only provide links. no additional text"},
+            {"role": "system", "content": "Be precise and concise and don't repeat. Only provide links,title and brief description of each resource. no additional text"},
             {"role": "user", "content": (
                 "Interesting resources to learn more about {}".format(topic)+
                 "Please output a JSON object containing the following fields: {}".format(media_prompt)
@@ -39,20 +33,22 @@ def tutorials(topic,media):
     response = requests.post(url, headers=headers, json=payload).json()
     
     res=response["choices"][0]["message"]["content"]
-    answer=res[res.index('{'):res.index("}")+1]
-    #print(answer)
-    answer=eval(answer)
+    
+    answer=res[res.index('{'):].strip().removesuffix("```")
+    print(answer)
+    answer=json.loads(answer)
+    #if(answer[-3:]=="```"):
+    #    answer=answer[:-3]
     #print(type(answer))
     #print(answer)
     res={}
+    return answer
     for i in answer:
-       # print(i)
-        if("links" in i.lower()):
-
+        if(True):#i in topic):
             #if(answer[i][0].count(' ')>0):
                 #continue
             res[i]=answer[i]
     print(res)
     return res
 
-tutorials("calculus",["videos","tutorials","images"])
+tutorials("oil painting",["videos","tutorials","images"])
